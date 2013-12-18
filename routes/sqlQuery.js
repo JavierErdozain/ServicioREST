fc1dcddf98e95392a9e987f492a654d92bf986a3
Javier Erdozain <fcojaviererdozainarlegui@Mac-mini-de-Fco.local>
2013-12-18 19:20:17 +0100

    Subida inicial del proyecto

---
+++ routes/sqlQuery.js
@@ -0,0 +1,15 @@
+
+/*
+ * GET users listing.
+ */
+ 
+var cnx = require('../mod/bd').conexion;
+
+exports.RecuperarInfoRecepcion = function(id, req, res){
+  var objConexion = new cnx;
+  objConexion.query('SELECT * FROM InfoRecepciones where IdRecepcion = ' +  +'', function(error, resultados){
+    objConexion.end();
+    console.log(resultados);
+    res.json({resultado: resultados });
+  });
+};
