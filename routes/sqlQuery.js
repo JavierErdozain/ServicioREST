a4f0955b2eaa89d9096b36d66e88f1ee15c38499
Javier Erdozain <fcojaviererdozainarlegui@Mac-mini-de-Fco.local>
2013-12-18 19:53:14 +0100

    .

--- routes/sqlQuery.js
+++ routes/sqlQuery.js
@@ -8,6 +8,7 @@ var cnx = require('../mod/bd').conexion;
 exports.RecuperarInfoRecepcion = function(req, res){
   var objConexion = new cnx;
   objConexion.query('SELECT * FROM InfoRecepciones where IdRecepcion = ' +  +'', function(error, resultados){
+  	console.log("Error: "+ error);
     objConexion.end();
     console.log(resultados);
     res.json({resultado: resultados });
